b6d678d7e7844ca81758cd1f919afbac3c116d92
abdulbaqi-sayani <abdulbaqialsayyani@gmail.com>
2026-02-16 21:44:38 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,11 +60,6 @@ android {
     }
     packagingOptions {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-}
-
 dependencies {
     implementation(project(":data"))
     implementation(project(":domain"))
