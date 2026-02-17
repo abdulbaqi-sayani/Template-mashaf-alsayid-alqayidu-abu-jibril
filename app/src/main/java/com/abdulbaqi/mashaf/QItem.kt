002fb48ba51b1edddf81dce16e40b26e155636c9
abdulbaqi-sayani <abdulbaqialsayyani@gmail.com>
2026-02-17 22:32:19 +0300

    Update QItem.kt

--- app/src/main/java/com/abdulbaqi/mashaf/QItem.kt
+++ app/src/main/java/com/abdulbaqi/mashaf/QItem.kt
@@ -1,12 +1,15 @@
 package com.abdulbaqi.mashaf
 
 sealed class QItem {
-    data class SurahTitle(val title: String, val surahIndex: Int) : QItem()
+
+    data class SurahTitle(
+        val name: String,
+        val surahIndex: Int
+    ) : QItem()
 
     data class Ayah(
         val text: String,
         val surahIndex: Int,
-        val ayahIndex: Int,
-        val number: Int? // ✅ null = بدون رقم
+        val ayahIndex: Int
     ) : QItem()
 }
