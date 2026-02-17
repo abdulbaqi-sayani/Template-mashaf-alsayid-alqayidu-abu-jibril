e494f87439e81e44d4834e66f2045f4500cb2257
abdulbaqi-sayani <abdulbaqialsayyani@gmail.com>
2026-02-17 21:10:54 +0300

    Create QItem.kt

---
+++ app/src/main/java/com/abdulbaqi/mashaf/QItem.kt
@@ -0,0 +1,6 @@
+package com.abdulbaqi.mashaf
+
+sealed class QItem {
+    data class SurahTitle(val title: String, val surahIndex: Int) : QItem()
+    data class Ayah(val text: String, val surahIndex: Int, val ayahIndex: Int) : QItem()
+}
