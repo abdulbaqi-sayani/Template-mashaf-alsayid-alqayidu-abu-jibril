0fb739e9dc61a1c9151a8f8e12cf84217f8e825a
abdulbaqi-sayani <abdulbaqialsayyani@gmail.com>
2026-02-21 01:16:50 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("com.android.application") version "7.0.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
 }
