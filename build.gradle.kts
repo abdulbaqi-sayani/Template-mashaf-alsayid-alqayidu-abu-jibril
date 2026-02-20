8da3d9642413238ccd085ae2ff095f2e30e60018
abdulbaqi-sayani <abdulbaqialsayyani@gmail.com>
2026-02-21 00:31:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    ext {
+        set("kotlin_version", "1.8.22")
+    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath(BuildPlugins.android)
-        classpath(BuildPlugins.kotlin)
-        classpath(BuildPlugins.hilt)
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
     }
-}
-
-task("clean", Delete::class) {
-    delete = setOf(rootProject.buildDir)
 }
