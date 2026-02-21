4229c077f826d17dd1dd478c49016b866aefc2ca
abdulbaqi-sayani <abdulbaqialsayyani@gmail.com>
2026-02-21 21:14:50 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,35 +30,23 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
-        debug {
-            isMinifyEnabled = false
-            isShrinkResources = false
-        }
     }
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
 
-    // ✅ لحل أخطاء layout_constraint*
+    // 🔥 مهم جدًا لحل layout_constraint*
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-
-    // ✅ لحل lifecycleScope
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-
-    // ✅ لحل Dispatchers / withContext / kotlinx
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
-```0
