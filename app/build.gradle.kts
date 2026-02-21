b4a2c874c0448bb5914874ba659fd5e1f0d38c79
abdulbaqi-sayani <abdulbaqialsayyani@gmail.com>
2026-02-21 22:19:19 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
 }
 
 android {
-
-    namespace = "com.abdulbaqi.mashaf"   // 🔥 مهم جداً
+    namespace = "com.abdulbaqi.mashaf"
     compileSdk = 34
 
     defaultConfig {
@@ -42,10 +41,9 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
